e2e3ae9888f280b0b2360c51a2250ea8f00ef70d
Martin Kuettler <martin.kuettler@kernkonzept.com>
2023-10-26 10:20:15 +0200

    Removed uvmm and virtio-net

--- src/l4/pkg/uvmm/server/src/ARCH-amd64/cpu_dev.cc
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only or License-Ref-kk-custom */
-/*
- * Copyright (C) 2016-2017, 2022 Kernkonzept GmbH.
- * Author(s): Philipp Eppelt <philipp.eppelt@kernkonzept.com>
- */
-
-#include "cpu_dev.h"
