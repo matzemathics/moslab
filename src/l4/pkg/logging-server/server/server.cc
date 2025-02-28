abf40b82fcf1ca32c26c20306e37fa02b4b58c71
Matthias Meissner <geige.matze@gmail.com>
2025-02-28 13:39:52 +0100

    use std::string for tag

--- src/l4/pkg/logging-server/server/server.cc
+++ src/l4/pkg/logging-server/server/server.cc
@@ -6,11 +6,15 @@
 
 #include <l4/logging-server/logging.h>
 
+#include <string>
+
 #define MAX_TAG_LEN 20
 
 static L4Re::Util::Registry_server<> server;
 
 struct LoggingServer: L4::Epiface_t<LoggingServer, Logger> {
+    LoggingServer(const char *name) : name(name) {}
+
     int op_log(Logger::Rights rights, LoggingBuffer &args)
     {
         (void)rights;
@@ -22,17 +26,12 @@ struct LoggingServer: L4::Epiface_t<LoggingServer, Logger> {
 
         args.msg[args.len] = '\0';
 
-        printf("[%s]: %s\n", name, args.msg);
+        printf("[%s]: %s\n", name.c_str(), args.msg);
         return 0;
     }
 
-    void set_name(char const* new_name) {
-        strncpy(this->name, new_name, MAX_TAG_LEN - 1);
-        name[MAX_TAG_LEN - 1] = 0;
-    }
-
 private:
-    char name[MAX_TAG_LEN] = "unnamed";
+    std::string name;
 };
 
 struct SessionServer: L4::Epiface_t<SessionServer, L4::Factory> {
@@ -55,9 +54,7 @@ struct SessionServer: L4::Epiface_t<SessionServer, L4::Factory> {
         if (strlen(tag.value<char const*>()) > MAX_TAG_LEN)
             return - L4_EINVAL;
 
-        auto instance = new LoggingServer();
-
-        instance->set_name(tag.value<char const*>());
+        auto instance = new LoggingServer(tag.value<char const*>());
         server.registry()->register_obj(instance);
 
         res = L4::Ipc::make_cap_rw(instance->obj_cap());
